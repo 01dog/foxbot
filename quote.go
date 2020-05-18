894cfe9295a7867eb626028f335cce28918dbe9f
01dog <bellwitch@pm.me>
2020-05-18 00:09:37 -0400

    *shrug*

--- quote.go
+++ quote.go
@@ -12,35 +12,22 @@ import (
 
 // TODO: a lot needs to be fixed here
 // better handling when the quotes db is empty/0 for the ID
-// lastID needs to not be init with 1, this is a temp fix because of how i spaghetti'd this together
 // need a way to list IDs and quotes to delete
 // select a quote by ID
-
-var lastID int
 
 func init() {
 	NewCommand("quote", false, getQuote).SetHelp("get a random quote").Add()
 	NewCommand("addquote", false, addQuote).SetHelp("add quote").ReqAdmin().Add()
-	NewCommand("remquote", false, remQuote).SetHelp("remove quote by id").ReqAdmin().Add()
+	// NewCommand("remquote", false, remQuote).SetHelp("remove quote by id").ReqAdmin().Add()
+	// NewCommand("listquotes", false, listQuotes).SetHelp("list quotes by id").ReqAdmin().Add()
 	initDB()
-	lastID = 1
 }
 
 func getQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
 	database, _ := sql.Open("sqlite3", "./quotes.db")
 	var quote string
 
-	rows, _ := database.Query("SELECT MAX(rowid) FROM quotes") // LAST_INSERT_ROWID() would be better but this is fine
-	for rows.Next() {                                          // is this how to do this?
-		rows.Scan(&lastID)
-	}
-
-	randomID := GenRandomNum(lastID)
-	if randomID == 0 { // feels gross
-		randomID++
-	}
-
-	rows, _ = database.Query("SELECT quote FROM quotes WHERE rowid=?", randomID)
+	rows, _ := database.Query("SELECT quote FROM quotes ORDER BY RANDOM() LIMIT 1;")
 	for rows.Next() {
 		rows.Scan(&quote)
 		s.ChannelMessageSend(m.ChannelID, quote)
@@ -48,6 +35,11 @@ func getQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string
 }
 
 func addQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
+	if len(msgList) < 2 {
+		s.ChannelMessageSend(m.ChannelID, "usage: >addquote QUOTE")
+		return
+	}
+
 	database, _ := sql.Open("sqlite3", "./quotes.db")
 	quoteToAdd := strings.Join(msgList[1:], " ")
 
@@ -55,24 +47,24 @@ func addQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string
 	statement.Exec(quoteToAdd)
 }
 
-func remQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
-	if len(msgList) < 2 {
-		s.ChannelMessageSend(m.ChannelID, "usage: >remquote ID")
-		return
-	}
-	database, _ := sql.Open("sqlite3", "./quotes.db")
-	ID, _ := StrArrayToInt(msgList[1:]) // this and the following line *need* to be changed, this isn't a good way to do this
-	if ID[0] > lastID || ID[0] == 0 {
-		s.ChannelMessageSend(m.ChannelID, "ID not found, try again.")
-	}
+// func remQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
+// 	if len(msgList) < 2 {
+// 		s.ChannelMessageSend(m.ChannelID, "usage: >remquote ID")
+// 		return
+// 	}
 
-	statement, _ := database.Prepare("DELETE FROM quotes WHERE rowid=?")
-	statement.Exec(ID[0])
-}
+// 	database, _ := sql.Open("sqlite3", "./quotes.db")
+// 	ID, _ := StrArrayToInt(msgList[1:]) // this and the following line *need* to be changed, this isn't a good way to do this
+// 	if ID[0] > lastID || ID[0] == 0 {
+// 		s.ChannelMessageSend(m.ChannelID, "ID not found, try again.")
+// 	}
 
-func listQuote(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
-	// placeholder
-}
+// 	statement, _ := database.Prepare("DELETE FROM quotes WHERE rowid=?")
+// 	statement.Exec(ID[0])
+// }
+
+// func listQuotes(s *discordgo.Session, m *discordgo.MessageCreate, msgList []string) {
+// }
 
 // checking if the quote db exists and if not, create it
 func initDB() {
@@ -82,7 +74,7 @@ func initDB() {
 	} else if os.IsNotExist(err) {
 		fmt.Println("db does not exist, creating")
 		database, _ := sql.Open("sqlite3", "./quotes.db")
-		statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS quotes (quote TEXT)")
+		statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS quotes (id INTEGER PRIMARY KEY, quote TEXT)")
 		statement.Exec()
 	} else {
 		// typically it shouldnt reach this code but in the event that os.Stat() does something weird this should 'catch' it
