4e1d6aa0898767a0e3121659257a44a32123b600
01dog <bellwitch@pm.me>
2020-08-15 13:31:26 -0400

    added regex to filter out bad word thanks to Gust

--- events.go
+++ events.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -11,6 +12,16 @@ func EvalMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// ignore the message if its from the bot
 	if m.Author.ID == s.State.User.ID {
 		return
+	}
+
+	// regex to match the N word
+	// this is because of you, Gust. . .
+	r, _ := regexp.Compile("(?i)n[aei1]+(g|6)+(ger|ga|a|6er|g3r|64|4|63r|e|r|ge|gr)[sz]*")
+	message := strings.Split(m.Content, " ")
+	joinedMessage := strings.Join(message, "")
+
+	if match := r.Match([]byte(joinedMessage)); match {
+		s.ChannelMessageDelete(m.ChannelID, m.ID)
 	}
 
 	// ignore the message if it doesnt have the command prefix
